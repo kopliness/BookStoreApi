ab0c629378c5137eee302d934a52ce7ced8a8fab
kopliness <mr.vladlis3568@gmail.com>
2024-05-28 17:21:59 +0300

    automatic addition of records as an example

--- MongoDbContext.cs
+++ MongoDbContext.cs
@@ -1,5 +1,6 @@
-using BookStoreApi.Models;
 using MongoDB.Driver;
+using BookStoreApi.Models;
+
 namespace BookStoreApi;
 
 public class MongoDbContext
@@ -11,6 +12,8 @@ public class MongoDbContext
         var client = new MongoClient("mongodb://mongodb:27017");
         if (client != null)
             _database = client.GetDatabase("BookStoreDb");
+
+        CreateInitialRecords();
     }
 
     public IMongoCollection<Book> Books
@@ -18,6 +21,32 @@ public class MongoDbContext
         get
         {
             return _database.GetCollection<Book>("Book");
+        }
+    }
+
+    private void CreateInitialRecords()
+    {
+        var books = Books.Find(_ => true).ToList();
+
+        if (!books.Any())
+        {
+            Books.InsertOne(new Book
+            {
+                Id = "61a6058e6c43f32854e51f51",
+                BookName = "Design Patterns",
+                Price = (decimal)54.93,
+                Category = "Computers",
+                Author = "Ralph Johnson"
+            });
+
+            Books.InsertOne(new Book
+            {
+                Id = "61a6058e6c43f32854e51f52",
+                BookName = "Clean Code",
+                Price = (decimal)43.15,
+                Category = "Computers",
+                Author = "Robert C. Martin"
+            });
         }
     }
 }
